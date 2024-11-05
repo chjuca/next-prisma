e471f465a3436c79c2ae85f1a280682fb946f641
chjuca <junio55.jj@gmail.com>
2024-11-05 15:39:15 -0500

    ✨ Add form and list view to tasks

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,7 +1,51 @@
-function NewPage(){
+'use client'
+import { useRouter } from "next/navigation"
+function NewPage() {
+
+    const router = useRouter();
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        const title = e.target.title.value
+        const description = e.target.description.value
+
+        const res = await fetch('/api/tasks', {
+            method: 'POST',
+            body: JSON.stringify({title, description}),
+            headers: {
+                "Content-Type": 'application/json'
+            }
+        })
+
+        const data = await res.json()
+        router.push("/")
+    }
+
     return (
-        <div>
-            Formulario de Tareas
+        <div className="h-screen flex justify-center items-center">
+            <form className="bg-slate-800 p-10 w1/4"
+                onSubmit={onSubmit}
+            >
+                <label htmlFor="title"
+                    className="font-bold text-sm"
+                > Title:</label>
+                <input type="text" 
+                    className="border border-gray-400 p-2 mb-4 w-full text-black"
+                    placeholder="Title"
+                    id="title"
+                />
+                <label htmlFor="description"
+                    className="font-bold text-sm"
+                > Description:</label>
+                <textarea rows="3"
+                    className="border border-gray-400 p-2 mb-4 w-full text-black"
+                    placeholder="Write description"
+                    id="description"
+                ></textarea>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px4 rounded">
+                    Create
+                </button>
+            </form>
         </div>
     )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,26 @@
-export default function Home() {
+
+
+async function loadTasks() {
+    const res = await fetch("http://localhost:3000/api/tasks")
+    const data = await res.json()
+    return data
+}
+export default async function HomePage() {
+
+  const tasks = await loadTasks()
   return (
-    <div>
-      Tareas
-    </div>
+    <section className="container mx-auto">
+      <div className="grid grid-col-3">
+        {
+          tasks.map(task => (
+            <div key={task.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
+              <h3 className="font-bold text-3xl mb-2">{task.title}</h3>
+              <p>{task.description}</p>
+              <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+            </div>
+          ))
+        }
+      </div>
+    </section>
   );
 }
